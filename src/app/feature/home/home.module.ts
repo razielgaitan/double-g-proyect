a3e5b917ddcb48dba36815aef3abbded655ab2fa
razielsantex <raziel.gaitan@santexgroup.com>
2023-12-08 17:11:23 -0300

    first commit

--- src/app/feature/home/home.module.ts
+++ src/app/feature/home/home.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,6 +13,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
     CommonModule,
     HomeRoutingModule,
     SharedModule,
+    FontAwesomeModule,
   ]
 })
 export class HomeModule { }
