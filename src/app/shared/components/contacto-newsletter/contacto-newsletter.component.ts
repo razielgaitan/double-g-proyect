534c8c73d31d69369176f9e47d35e9b8ba1c5b11
Maxi <maxirivadero2000@gmail.com>
2023-12-10 01:57:41 -0300

    Contacto-News con animaciones, falta agregar los form y que sea con click no hover

---
+++ src/app/shared/components/contacto-newsletter/contacto-newsletter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacto-newsletter',
+  templateUrl: './contacto-newsletter.component.html',
+  styleUrls: ['./contacto-newsletter.component.scss']
+})
+export class ContactoNewsletterComponent {
+
+}
