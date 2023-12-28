1ac0c11a9c609814ac52ee01f0496bb8d7df7288
Ivan <ivanlopezokk@gmail.com>
2023-12-27 21:42:47 -0300

    global alojamiento added

--- src/app/feature/alojamientos/alojamientos-routing.module.ts
+++ src/app/feature/alojamientos/alojamientos-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AlojamientosComponent } from './alojamientos/alojamientos.component';
@@ -11,8 +11,10 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+   
   ],
   exports: [RouterModule],
+
 })
   
 export class AlojamientosRoutingModule { }
--- src/app/feature/alojamientos/alojamientos.module.ts
+++ src/app/feature/alojamientos/alojamientos.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { AlojamientosRoutingModule } from './alojamientos-routing.module';
 import { AlojamientosComponent } from './alojamientos/alojamientos.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { NgIconsModule } from '@ng-icons/core'
+import { ionBusinessOutline, ionPeopleOutline, ionColorWandOutline } from '@ng-icons/ionicons';
 
 @NgModule({
   declarations: [AlojamientosComponent],
@@ -10,6 +12,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
     CommonModule,
     AlojamientosRoutingModule,
     SharedModule,
+    NgIconsModule.withIcons({ ionBusinessOutline, ionPeopleOutline, ionColorWandOutline }),
   ]
 })
   
