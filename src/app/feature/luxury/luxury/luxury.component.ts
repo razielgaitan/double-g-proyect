883c30c460fc25cbe76a01e2cf44c949dbbf82bf
Maxi <maxirivadero2000@gmail.com>
2023-12-31 02:23:46 -0300

    intento de hacer vuelos

--- src/app/feature/luxury/luxury/luxury.component.ts
+++ src/app/feature/luxury/luxury/luxury.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./luxury.component.scss']
 })
 export class LuxuryComponent {
-
+  globalTexts: string[] = ['Diversos Destinos', 'Flexibilidad', 'Hermosos Lugares'];
+  globalDescs: string[] = ['Te ayudamos a buscar el auto que necesitas para tu viaje', 'Nos adaptamos a tus planes y tus decisiones!', 'Te brindamos un servicio constante solo para vos'];
+  globalIcons: string[] = ['ionCarSportOutline', 'ionCalendarOutline', 'ionOptionsOutline'];
 }
