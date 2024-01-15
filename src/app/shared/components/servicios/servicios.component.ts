a5aec766c1c23135e80c16df7055f1f9d7fb01b5
Maxi <maxirivadero2000@gmail.com>
2024-01-15 15:29:31 -0300

    animaciones

--- src/app/shared/components/servicios/servicios.component.ts
+++ src/app/shared/components/servicios/servicios.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
   styleUrls: ['./servicios.component.scss']
 })
-export class ServiciosComponent {
+export class ServiciosComponent implements AfterViewInit, OnDestroy {
+  private intersectionObserver: IntersectionObserver | undefined;
 
+  ngAfterViewInit() {
+    this.intersectionObserver = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.applyAnimations(entry.target as HTMLElement);
+          this.intersectionObserver?.unobserve(entry.target);
+        }
+      });
+    });
+
+    const sectionElement = document.getElementById('secondSection');
+
+    if (sectionElement) {
+      this.intersectionObserver?.observe(sectionElement);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.intersectionObserver) {
+      this.intersectionObserver.disconnect();
+    }
+  }
+
+  private applyAnimations(element: HTMLElement) {
+    element.classList.add('fade-in');
+  }
 }
