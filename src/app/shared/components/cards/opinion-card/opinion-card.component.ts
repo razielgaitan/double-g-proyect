51cf39f8ccecea52706a28431bfbcb3410d141fb
maxirivadero <maxirivadero2000@gmail.com>
2024-02-11 13:20:34 -0600

    ajuste de ancho de elementos e imagenes

--- src/app/shared/components/cards/opinion-card/opinion-card.component.ts
+++ src/app/shared/components/cards/opinion-card/opinion-card.component.ts
@@ -13,37 +13,37 @@ export class OpinionCardComponent implements OnInit, AfterViewInit {
       title: 'El mejor viaje...',
       date: '28 de Noviembre 2023',
       content: 'Gracias a Double G pude vivir una experiencia llena de emociones y conocer lugares mágicos.',
-      imageUrl: 'https://pbs.twimg.com/profile_images/890901007387025408/oztASP4n.jpg'
+      imageUrl: '../../../../../assets/images/opinion-cards/persona1.jpg'
     },
     {
       title: 'Experiencia única.',
       date: '15 de Octubre 2023',
       content: '¡Experiencia de viaje increíble con Double G! Gracias por hacer que mi viaje fuera inolvidable. ¡Altamente recomendado!',
-      imageUrl: 'https://pbs.twimg.com/profile_images/1645189316954529792/VqI0rH9N_400x400.jpg'
+      imageUrl: '../../../../../assets/images/opinion-cards/persona2.jpg'
     },
     {
       title: 'Increible',
       date: '2 de Marzo 2022',
       content: '¡Increíble servicio de Double G! Gracias por una experiencia de viaje excepcional.',
-      imageUrl: 'https://pbs.twimg.com/profile_images/1710773376698949632/_zuv7yws_400x400.jpg' 
+      imageUrl: '../../../../../assets/images/opinion-cards/persona3.jpg' 
     },
     {
       title: 'El mejor viaje...',
       date: '28 de Noviembre 2023',
       content: 'Gracias a Double G pude vivir una experiencia llena de emociones y conocer lugares mágicos.',
-      imageUrl: 'https://pbs.twimg.com/profile_images/890901007387025408/oztASP4n.jpg'
+      imageUrl: '../../../../../assets/images/opinion-cards/persona1.jpg'
     },
     {
       title: 'Experiencia única.',
       date: '15 de Octubre 2023',
       content: '¡Experiencia de viaje increíble con Double G! Gracias por hacer que mi viaje fuera inolvidable. ¡Altamente recomendado!',
-      imageUrl: 'https://pbs.twimg.com/profile_images/1645189316954529792/VqI0rH9N_400x400.jpg'
+      imageUrl: '../../../../../assets/images/opinion-cards/persona2.jpg'
     },
     {
       title: 'Increible',
       date: '2 de Marzo 2022',
       content: '¡Increíble servicio de Double G! Gracias por una experiencia de viaje excepcional.',
-      imageUrl: 'https://pbs.twimg.com/profile_images/1710773376698949632/_zuv7yws_400x400.jpg' 
+      imageUrl: '../../../../../assets/images/opinion-cards/persona3.jpg' 
     }
   ];
 
