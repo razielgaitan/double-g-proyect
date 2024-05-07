aff30dcbf672517ea3baa3e220cda38978c78d21
maxirivadero <maxirivadero2000@gmail.com>
2024-05-07 02:00:58 -0300

    corporativo

--- src/app/feature/corporativo/corporativo.module.ts
+++ src/app/feature/corporativo/corporativo.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CorporativoRoutingModule } from './corporativo-routing.module';
 import { CorporativoComponent } from './corporativo/corporativo.component';
@@ -11,6 +11,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
     CommonModule,
     CorporativoRoutingModule,
     SharedModule
-  ]
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CorporativoModule { }
--- src/app/feature/corporativo/corporativo/corporativo.component.ts
+++ src/app/feature/corporativo/corporativo/corporativo.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-corporativo',
   templateUrl: './corporativo.component.html',
   styleUrls: ['./corporativo.component.scss']
 })
-export class CorporativoComponent {
+export class CorporativoComponent  implements OnInit {
+  isScreenSmall = false;
+  isPhoneScreen = false;
 
+  ngOnInit(): void {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize(): void {
+    const screenWidth = window.innerWidth;
+
+    this.isPhoneScreen = screenWidth <= 768;
+    this.isScreenSmall = screenWidth <= 600;
+  }
 }
